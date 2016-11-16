944c748a6726aa5c8cd2105a5c0f966035256933
Kuntau <kuntau17@gmail.com>
2016-11-17 04:07:23 +0800

    change bootstrap-sass to bootstrap-stylus

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,7 +8,7 @@ window._ = require('lodash');
  */
 
 window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
+require('bootstrap-styl');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
@@ -29,17 +29,4 @@ Vue.http.interceptors.push((request, next) => {
     request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
 
     next();
-});
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-import Echo from "laravel-echo"
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'your-pusher-key'
 });
