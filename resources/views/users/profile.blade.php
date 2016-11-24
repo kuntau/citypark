2f9ee486d0cb6efb81ba00b39a813c6094fb097a
Kuntau <kuntau17@gmail.com>
2016-11-24 08:12:24 +0800

    update profile page

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -7,7 +7,30 @@
       <div class="panel panel-default">
         <div class="panel-heading">Profile</div>
         <div class="panel-body">
-          This is {{ $user->name }} profile registered with {{ $user->email }} email at {{ $user->created_at }}.
+          <form action="#" class="from-horizontal">
+            <div class="form-group">
+              <label for="" class="control-label">Name</label>
+              <input type="text" class="form-control" value="{{ $user->name }}" readonly>
+            </div>
+            <div class="form-group">
+              <label for="" class="control-label">E-mail</label>
+              <input type="text" class="form-control" value="{{ $user->email }}" readonly>
+            </div>
+            <div class="form-group">
+              <label for="" class="control-label">Company</label>
+              <input type="text" class="form-control" value="{{ $user->company_name }}" readonly>
+            </div>
+            <div class="form-group">
+              <label for="" class="control-label">Company Address</label>
+              <textarea class="form-control" row="3" readonly>{{ $user->company_address }}</textarea>
+            </div>
+            @if (isset($user->phone_office))
+              <div class="form-group">
+                <label for="" class="control-label">Phone (Office)</label>
+                <input type="text" class="form-control" value="{{ $user->phone_office }}" readonly>
+              </div>
+            @endif
+          </form>
         </div>
       </div>
     </div>
