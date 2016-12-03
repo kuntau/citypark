f8171bab2f3b10e1316b4d93224c063ec05550c7
Kuntau <kuntau17@gmail.com>
2016-12-04 03:35:07 +0800

    application history page

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -2,24 +2,46 @@
 
 @section('content')
 <div class="products container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <h2>Applications History</h2>
-            <ul class="list-group">
-              @foreach ($purchases as $application)
-                <li class="list-group-item">
-                  <h3>{{ $application->purpose }}</h3>
-                  <h4>Location : {{ $application->location }}</h4>
-                  <h5>Date : {{ date('d/m/Y', strtotime($application->from_at)).' ~ '.date('d/m/Y', strtotime($application->until_at)) }}</h5>
-                  <p>
-                    RM {{ $application->price }}<br />
-                    Approved: {{ $application->approved ? 'Yes' : 'No' }}<br />
-                    Paid: {{ $application->paid ? 'Yes' : 'No' }}<br />
-                  </p>
-                </li>
-              @endforeach
-            </ul>
-        </div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <h2>Applications History</h2>
+      <ul class="list-group">
+        @foreach ($purchases as $appl)
+          <li class="list-group-item">
+            <h3>{{ $appl->purpose }}</h3>
+            <h4>{{ $appl->location }}</h4>
+            <table class="table">
+              <tr>
+                <td>Date</td>
+                <td>{{ date('d/m/Y', strtotime($appl->from_at)).' ~ '.date('d/m/Y', strtotime($appl->until_at)) }}</td>
+              </tr>
+              <tr>
+                <td>No of Lot</td>
+                <td>{{ $appl->quantity_lot }}</td>
+              </tr>
+              <tr>
+                <td>Duration</td>
+                <td>{{ $appl->duration.($appl->duration > 1 ? ' days' : ' day') }}</td>
+              </tr>
+              <tr>
+                <td>Approved</td>
+                <td>{{ $appl->paid ? 'Yes' : 'No' }}</td>
+              </tr>
+              <tr>
+                <td>Paid</td>
+                <td>{{ $appl->paid ? 'Yes' : 'No' }}</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>RM{{ $appl->price }}</td>
+              </tr>
+            </table>
+            <button class="btn btn-primary">Make Payment</button>
+            <button class="btn btn-default">Delete Application</button>
+          </li>
+        @endforeach
+      </ul>
     </div>
+  </div>
 </div>
 @endsection
